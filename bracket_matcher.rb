848cd6768e329a305fd1d1ee0730572262ce9928
Max Ribbans <ribbans.max@gmail.com>
2021-04-09 12:13:40 -0400

    Add catch to ignore non-bracket characters

--- bracket_matcher.rb
+++ bracket_matcher.rb
@@ -5,7 +5,7 @@ def bracket(string)
   string.split('').each do |char|
     if open_brackets.include?(char)
       s << char
-    elsif open_brackets[closed_brackets.index(char)] == s[-1]
+    elsif closed_brackets.include?(char) and open_brackets[closed_brackets.index(char)] == s[-1]
       s.slice!(-1)
     end
   end
