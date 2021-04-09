09709d08635b54e251cd16f357c1c914636a3a1e
Max Ribbans <ribbans.max@gmail.com>
2021-04-09 12:25:30 -0400

    Add guard clause for first element being a closed bracket variety

--- bracket_matcher.rb
+++ bracket_matcher.rb
@@ -5,12 +5,12 @@ def bracket(string)
     '(' => ')'
   }
   s = ''
-  string.split('').each do |char|
+  brackets[string[0]] ? string.split('').each do |char|
     if brackets[char]
       s << char
     elsif brackets.invert[char] == s[-1]
       s.slice!(-1)
     end
-  end
+  end : s = '.'
   s.length == 0
 end
